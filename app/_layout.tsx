71e8978adeed49a067480501556b782e3842b32b
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-20 22:49:57 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'; import { useFonts } from 'expo-font'; import { Stack, useRouter } from 'expo-router'; import { StatusBar } from 'expo-status-bar'; import 'react-native-reanimated'; import { useEffect, useState } from 'react'; import { isAuthenticated } from '../lib/auth'; import { useColorScheme } from '@hooks/useColorScheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'; import { useFonts } from 'expo-font'; import { Stack, useRouter } from 'expo-router'; import { StatusBar } from 'expo-status-bar'; import 'react-native-reanimated'; import { useEffect, useState } from 'react'; import { isAuthenticated } from '../lib/auth'; 
+import { useColorScheme } from 'react-native';
 
 export default function RootLayout() { const colorScheme = useColorScheme(); const [loaded] = useFonts({ SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'), });
 
