3c6d12e275cbc9d0aa646c6de7a81afab0733092
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-21 08:33:01 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://192.168.100.15:3000",
+  baseURL: "http://192.168.100.155:3000",
   headers: {
     'Content-Type': 'application/json',
   },
