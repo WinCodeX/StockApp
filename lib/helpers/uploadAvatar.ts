a6bb748f3c6af794bfc590fa8f39b6f77b6f97a6
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-22 08:26:48 +0300

    Create uploadAvatar.ts

---
+++ lib/helpers/uploadAvatar.ts
@@ -0,0 +1,24 @@
+// lib/helpers/uploadAvatar.ts
+import api from "../api";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+
+export const uploadAvatar = async (uri: string) => {
+  const token = await SecureStore.getItemAsync("auth_token");
+  const form = new FormData();
+
+  form.append("avatar", {
+    uri,
+    name: "avatar.jpg",
+    type: Platform.OS === "ios" ? "image/jpeg" : "image/jpg",
+  } as any);
+
+  const res = await api.post("/me/avatar", form, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return res.data;
+};
