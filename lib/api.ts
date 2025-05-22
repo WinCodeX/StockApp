411ea6ed9a8e325752d6406a4e84da25a64eef7c
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-22 08:19:58 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,12 +1,20 @@
 // lib/api.ts
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 
 const api = axios.create({
-  baseURL: "http://192.168.100.155:3000",
+  baseURL: 'http://192.168.100.155:3000',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default api;
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync('auth_token');
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
+export default api;
