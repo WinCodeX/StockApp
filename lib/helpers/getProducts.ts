467f0eb75eb81b30c28066db6ed5f993866a48cc
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-24 19:14:06 +0300

    Update getProducts.ts

--- lib/helpers/getProducts.ts
+++ lib/helpers/getProducts.ts
@@ -1,13 +1,28 @@
 // lib/helpers/getProducts.ts
+
 import api from "../api";
 import * as SecureStore from "expo-secure-store";
 
-export const getProducts = async () => {
+export const getProducts = async (
+  page: number = 1,
+  perPage: number = 10,
+  query: string = ""
+) => {
   const token = await SecureStore.getItemAsync("auth_token");
+
   const res = await api.get("/api/v1/products", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
+    params: {
+      page,
+      per_page: perPage,
+      query,
+    },
   });
-  return res.data.products.data;
+
+  return {
+    products: res.data.products.data,
+    meta: res.data.meta,
+  };
 };
