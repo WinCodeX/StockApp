62f3df3480d131f7f15146780bcf562225c84e03
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-05-28 16:58:24 +0300

    Update getProductStats.ts

--- lib/helpers/getProductStats.ts
+++ lib/helpers/getProductStats.ts
@@ -1,10 +1,18 @@
 import api from '../api';
 import * as SecureStore from 'expo-secure-store';
+import NetInfo from '@react-native-community/netinfo';
 
 export const getProductStats = async () => {
+  const { isConnected } = await NetInfo.fetch();
+  if (!isConnected) throw new Error('No internet connection');
+
   const token = await SecureStore.getItemAsync('auth_token');
+
   const res = await api.get('/api/v1/products/stats', {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
+
   return res.data;
 };
