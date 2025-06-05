e54176a4f9b81d627579e457b43915fed7b9dd09
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-06-05 20:02:19 +0300

    Update stockHistory.ts
    fix

--- lib/helpers/stockHistory.ts
+++ lib/helpers/stockHistory.ts
@@ -1,11 +1,10 @@
 // lib/helpers/stockHistory.ts
-
 import api from '../api';
 
 export const getStockHistory = async (productId: number) => {
   try {
-    const res = await api.get(`/api/v1/products/${productId}/stocks`);
-    return res.data.data; // adjust if your response structure is different
+    const res = await api.get(`/api/v1/products/${productId}/history`);
+    return res.data;
   } catch (error: any) {
     console.error('Error fetching stock history:', error.response?.data || error.message);
     throw error;
